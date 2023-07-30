b619febf79d0a1b2a8c7c7a04af5f8c14f3cb7d9
maxpopovych <vwotar@gmail.com>
2023-07-30 18:08:23 +0200

    v2 wip

--- src/main/kotlin/com/makki/exchanges/models/KlineAsset.kt
+++ src/main/kotlin/com/makki/exchanges/models/KlineAsset.kt
@@ -12,6 +12,28 @@ interface Kline {
 	val close: Double
 	val volume: Double
 	val trades: Int
+
+	fun copy(
+		start: Long? = null,
+		end: Long? = null,
+		open: Double? = null,
+		high: Double? = null,
+		low: Double? = null,
+		close: Double? = null,
+		volume: Double? = null,
+		trades: Int? = null,
+	): Kline {
+		return KlineAsset(
+			start = start ?: this.start,
+			end = end ?: this.end,
+			open = open ?: this.open,
+			high = high ?: this.high,
+			low = low ?: this.low,
+			close = close ?: this.close,
+			volume = volume ?: this.volume,
+			trades = trades ?: this.trades
+		)
+	}
 }
 
 @Serializable
